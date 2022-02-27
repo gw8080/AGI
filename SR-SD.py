65e38a8f6d0266bd57fe85da6711bf5cc10a1439
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-02-27 16:22:18 +1100

    Add files via upload

--- SR-SD.py
+++ SR-SD.py
@@ -112,12 +112,14 @@ if option == "load":
     prev = ""
     while(True):
         i += 1
-        #if do then random actions for associative reasoning
+        #if experiment then random actions for associative reasoning
+        action = round(random.uniform(0, 10))
+        print("doing action " + str(action) + " to experiment and learn.")
         image = pyautogui.screenshot()
         image = cv2.cvtColor(np.array(image),
                      cv2.COLOR_RGB2BGR)
-        cv2.imwrite("frame.png", image)
-        sunflower_path = 'Frame.png'
+        cv2.imwrite("frame_" + str(i) + "_action_" + str(action) + ".png", image)
+        sunflower_path = "frame_" + str(i) + "_action_" + str(action) + ".png"
         img = tf.keras.utils.load_img(
         sunflower_path, target_size=(img_height, img_width)
         )
@@ -129,7 +131,7 @@ if option == "load":
         "This image most likely belongs to {} with a {:.2f} percent confidence."
         .format(class_names[np.argmax(score)], 100 * np.max(score))
         )
-        #save association
+        # search directory structure for stored action frame from action-vision association then do.
         sleep(1)
         if (i % 2) != 0:
             if user_inputB == "exec":
@@ -137,13 +139,76 @@ if option == "load":
                 tokenizer = GPT2Tokenizer.from_pretrained('./cached_t-GPT2')
                 inputs = tokenizer.encode(class_names[np.argmax(score)], return_tensors='pt')
                 outputs = modelM.generate(
-                inputs, max_length=128, do_sample=True, temperature=5.0
+                inputs, max_length=16, do_sample=True, temperature=5.0
                 )
                 string = tokenizer.decode(outputs[0], skip_special_tokens=True)
                 prev = class_names[np.argmax(score)]
         if (i % 2) == 0:
             if user_inputB == "exec":
-                if string.find(class_names[np.argmax(score)]) > len(prev):
-                        print("Success")
-                        #if futher research of process, use stored actions from associative reasoning
-                        break
+                if string.find(class_names[np.argmax(score)]) < len(prev): # '<' test mode, '>' real mode
+                        print("Success in thinking, stored action frame")
+                        cv2.imwrite(class_names[np.argmax(score)] + "_frame_success_" + str(i) + "_action_" + str(action) + ".png", image)
+                        # action-vision association
+                        process = string.split(" ")
+                        for word in process:
+                            print("downloading " + word + " images for training purposes.")
+                            #download images to storage...
+                        print("self improving...")
+                        data_dir = pathlib.Path("C:\\Users\\George\\.keras\\datasets\\DB")
+                        image_count = len(list(data_dir.glob('*/*.jpg')))
+                        print(image_count)
+                        batch_size = 32
+                        img_height = 180
+                        img_width = 180
+                        train_ds = tf.keras.utils.image_dataset_from_directory(
+                        data_dir,
+                        validation_split=0.2,
+                        subset="training",
+                        seed=123,
+                        image_size=(img_height, img_width),
+                        batch_size=batch_size)
+                        val_ds = tf.keras.utils.image_dataset_from_directory(
+                        data_dir,
+                        validation_split=0.2,
+                        subset="validation",
+                        seed=123,
+                        image_size=(img_height, img_width),
+                        batch_size=batch_size)
+                        class_names = train_ds.class_names
+                        print(class_names)
+                        AUTOTUNE = tf.data.AUTOTUNE
+                        train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+                        val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+                        normalization_layer = layers.Rescaling(1./255)
+                        normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+                        image_batch, labels_batch = next(iter(normalized_ds))
+                        first_image = image_batch[0]
+                        num_classes = len(class_names)
+                        model = Sequential([
+                        layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+                        layers.Conv2D(16, 3, padding='same', activation='relu'),
+                        layers.MaxPooling2D(),
+                        layers.Conv2D(32, 3, padding='same', activation='relu'),
+                        layers.MaxPooling2D(),
+                        layers.Conv2D(64, 3, padding='same', activation='relu'),
+                        layers.MaxPooling2D(),
+                        layers.Flatten(),
+                        layers.Dense(128, activation='relu'),
+                        layers.Dense(num_classes)
+                        ])
+                        model.compile(optimizer='adam',
+                                    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                        metrics=['accuracy'])
+                        epochs=1
+                        history = model.fit(
+                        train_ds,
+                        validation_data=val_ds,
+                        epochs=epochs
+                        )
+                        acc = history.history['accuracy']
+                        val_acc = history.history['val_accuracy']
+                        loss = history.history['loss']
+                        val_loss = history.history['val_loss']
+                        model.save("improvement" + str(i))
+                        model = keras.models.load_model("improvement" + str(i))
+                        print("Loaded new model...")
